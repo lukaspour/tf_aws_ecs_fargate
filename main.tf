ade1adcf1c55cac4e596e2725620765b4e131dd1
Lukas Pour <lukaspour@gmail.com>
2019-10-23 10:00:47 +0200

    Add telia oss fargate as a module

--- main.tf
+++ main.tf
@@ -3,6 +3,16 @@
 module "ecs-fargate" {
   source  = "telia-oss/ecs-fargate/aws"
   version = "3.2.0"
-  # insert the 8 required variables here
+
+
+
+  cluster_id = ""
+  health_check = ""
+  lb_arn = ""
+  name_prefix = ""
+  private_subnet_ids = ""
+  task_container_image = ""
+  task_container_port = ""
+  vpc_id = ""
 }
 
